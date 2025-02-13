1d515d39749250f065ddd8e90670c61d0aa3bdc0
Yassine-X <baqudiryassine@gmail.com>
2025-02-13 14:27:42 +0100

    Thrid session updates

--- lib/app.dart
+++ lib/app.dart
@@ -7,22 +7,56 @@ class Test extends StatefulWidget {
   State<Test> createState() => _TestState();
 }
 
-class _TestState extends State<Test> {  
+class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text("Students Manager", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          backgroundColor: Color.fromARGB(255, 85, 20, 150),
           centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 23, 24, 24),
-        ),
-        body: TextField(
-          decoration: InputDecoration(
-            hintText: "Name",
+          title: const Text(
+            "Students Manager",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
+        body: const Row(
+          children: [
+            SimpleTextField(hintText: "Name",),
+            SimpleTextField(hintText: "Age", textInputType: TextInputType.number,),
+            SimpleTextField(hintText: "Branch",),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 152, 205, 17),
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ));
+  }
+}
+
+class SimpleTextField extends StatelessWidget {
+ 
+  final TextInputType? textInputType;
+  final String? hintText;
+  const SimpleTextField({
+    this.textInputType,
+    required this.hintText,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextField(
+        keyboardType: textInputType,
+        decoration: InputDecoration(
+          hintText: hintText,
+        ),
       ),
-    ); 
+    );
   }
 }
